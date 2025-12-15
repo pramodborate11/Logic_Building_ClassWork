d0223aac846a5a39e4647678320a89a8429bd753
Pramod Borate <147588311+pramodborate11@users.noreply.github.com>
2025-12-15 18:12:10 +0530

    Add files via upload

---
+++ program27.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+  
+//Dynamic Function
+
+void Display(int iFrequency)   
+{
+    int iCnt = 0;
+
+    //FILTER
+    if(iFrequency <0)
+    {
+        printf("Invalid input");
+        return;
+    }
+    for(iCnt = 1; iCnt <= iFrequency; iCnt++)
+    {
+        printf("Jay Ganesh...\n");
+    }
+     
+}
+
+
+int main()
+{
+    int iCount = 0;
+
+    printf("Enter the frequency :\n");
+    scanf("%d",&iCount);
+
+    Display(iCount);
+
+    return 0;
+}
