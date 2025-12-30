123f883305286c0255358c5a81f45c22ff0cf550
Pramod Borate <147588311+pramodborate11@users.noreply.github.com>
2025-12-30 12:22:45 +0530

    Add files via upload

---
+++ program188.java
@@ -0,0 +1,65 @@
+/*
+   iRow : 6
+   iCol : 6
+
+out put : $ $ $ $ $ $
+          $ $ * * * $ 
+          $ * $ * * $ 
+          $ * * $ * $ 
+          $ * * * $ $ 
+          $ $ $ $ $ $
+          
+*/       
+
+
+import java.util.Scanner;
+
+class Pattern
+{
+    //FILTER FOR SQUARE MATRIX
+   public void Display(int iRow,int iCol)
+   {
+      int i = 0, j = 0;
+      
+      for(i = 1; i <= iRow; i++)
+      {
+        if(iRow != iCol)
+        {
+            System.out.println("Invalid Input");
+            System.out.println("Row number and coloumn number should be same");
+            return;
+        }
+         for(j = 1; j <= iCol; j++)
+         {
+            if(i == j || i == 1|| i == iRow || j == 1 || j == iCol)
+            {
+                System.out.printf("$\t");  
+            }
+            else
+            {
+                System.out.printf("*\t");
+            }
+            
+         }
+         System.out.println();
+      }
+   }
+
+}
+class program188
+{
+    public static void main(String A[])
+    {
+        Scanner sobj = new Scanner(System.in);
+        int iValue1 = 0,iValue2 = 0;
+
+        System.out.println("Enter number of Rows :");
+        iValue1 = sobj.nextInt();
+
+        System.out.println("Enter number of Coloumn :");
+        iValue2 = sobj.nextInt();
+        
+        Pattern pobj = new Pattern();
+        pobj.Display(iValue1, iValue2);
+    }
+}
---
+++ program195.java
@@ -0,0 +1,55 @@
+/*
+   iRow : 4
+   iCol : 4
+
+out put : *
+          * *
+          * * *
+          * * * *
+*/       
+
+
+import java.util.Scanner;
+
+class Pattern
+{
+    //FILTER FOR SQUARE MATRIX
+   public void Display(int iRow,int iCol)
+   {
+      int i = 0, j = 0;
+      
+      for(i = 1; i <= iRow; i++)
+      {
+        if(iRow != iCol)
+        {
+            System.out.println("Invalid Input");
+            System.out.println("Row number and coloumn number should be same");
+            return;
+        }
+         for(j = 1; i <= j; j++)
+          {
+            System.out.print("*\t");  
+          }
+        }
+         System.out.println();
+    }
+}
+
+
+class program195
+{
+    public static void main(String A[])
+    {
+        Scanner sobj = new Scanner(System.in);
+        int iValue1 = 0,iValue2 = 0;
+
+        System.out.println("Enter number of Rows :");
+        iValue1 = sobj.nextInt();
+
+        System.out.println("Enter number of Coloumn :");
+        iValue2 = sobj.nextInt();
+        
+        Pattern pobj = new Pattern();
+        pobj.Display(iValue1, iValue2);
+    }
+}
